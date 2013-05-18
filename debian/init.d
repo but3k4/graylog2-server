66b6d772ba7dbc2b631ad86183bc42d4727e5cbb
but3k4 <cbsfilho@gmail.com>
2013-05-18 19:29:44 -0300

    new upstream release. Update for wheezy

--- debian/init.d
+++ debian/init.d
@@ -5,7 +5,8 @@
 # Required-Stop:     $network $local_fs $syslog $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Starts Graylog2 server
+# Should-Stop:       $local_fs
+# Short-Description: Starts Graylog2-server
 # Description:       Graylog2 is an open source syslog implementation that
 #                    stores your logs in ElasticSearch. It consists of a
 #                    server written in Java that accepts your syslog messages
@@ -16,15 +17,17 @@
 
 # Author: Claudio Filho <claudio.filho@locaweb.com.br>
 
-PATH=/bin:/usr/bin:/sbin:/usr/sbin
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
+USER=graylog2
+GROUP=graylog2
 NAME=graylog2-server
-DESC=graylog2-server
+DESC="Graylog2 Server"
 DEFAULT=/etc/default/$NAME
 
-if [ `id -u` -ne 0 ]; then
-   echo "You need root privileges to run this script"
-   exit 1
-fi
+DIR=/usr/share/java
+PID_FILE=/var/tmp/graylog2.pid
+JAVA_OPTS="-Xms512m -Xmx512m -Xss1024k -XX:MaxPermSize=1g"
+GRAYLOG2_OPTS="-f /etc/graylog2/graylog2.conf -p $PID_FILE"
 
 # Load the VERBOSE setting and other rcS variables
 . /lib/init/vars.sh
@@ -33,8 +36,10 @@ fi
 # Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
 . /lib/lsb/init-functions
 
+set -e
+
 # The first existing directory is used for JAVA_HOME (if JAVA_HOME is not defined in $DEFAULT)
-JDK_DIRS="/usr/lib/jvm/java-7-oracle /usr/lib/jvm/java-6-sun /usr/lib/jvm/java-7-openjdk /usr/lib/jvm/java-6-openjdk /usr/lib/jvm/java-7-openjdk-amd64/ /usr/lib/jvm/java-6-openjdk-amd64/"
+JDK_DIRS="/usr/lib/jvm/java-7-oracle /usr/lib/jvm/java-7-openjdk /usr/lib/jvm/java-7-openjdk-amd64/ /usr/lib/jvm/java-6-openjdk /usr/lib/jvm/java-6-openjdk-amd64/"
 
 # Look for the right JVM to use
 for jdir in $JDK_DIRS; do
@@ -44,20 +49,10 @@ for jdir in $JDK_DIRS; do
 done
 export JAVA_HOME
 
-# The following variables can be overwritten in $DEFAULT
-
-DIR=/usr/share/java
-JAVA_OPTS="-Xms2g -Xmx2g -Xss256k -XX:MaxPermSize=1g"
-PIDFILE=/var/run/$NAME.pid
-
-# End of variables that can be overwritten in $DEFAULT
-
 # overwrite settings from default file
 if [ -f "$DEFAULT" ]; then
     . "$DEFAULT"
 fi
-
-set -e
 
 case "$1" in
   start)
@@ -66,63 +61,32 @@ case "$1" in
         exit 1
     fi
 
-    log_daemon_msg "Starting $DESC: "
-
-    if [ -f "$PIDFILE" ] && [ $(ps -o pid --no-headers -p `cat $PIDFILE`) ]; then
-        log_success_msg "$DESC is running with pid `cat $PIDFILE`"
+    log_daemon_msg "Starting $DESC"
+    if [ -f "$PID_FILE" ] && [ $(ps -o pid --no-headers -p `cat $PID_FILE`) ]; then
         exit 1
     fi
 
-    if start-stop-daemon -S -q -b -o --exec $JAVA_HOME/bin/java -- $JAVA_OPTS -jar $DIR/${NAME}.jar -p $PIDFILE; then
+    if start-stop-daemon -S -q -b -o --user $USER -c $USER --exec $JAVA_HOME/bin/java -- $JAVA_OPTS -jar $DIR/${NAME}.jar $GRAYLOG2_OPTS; then
+        log_end_msg 0
+    else
+        log_end_msg 1
+    fi
+    ;;		
+  stop)
+    log_daemon_msg "Stopping $DESC"
+    if start-stop-daemon -K -q -o --user $USER -c $USER --pidfile "$PID_FILE"; then
         log_end_msg 0
     else
         log_end_msg 1
     fi
     ;;
-  stop)
-    log_daemon_msg "Stopping $DESC"
-
-    if [ -f "$PIDFILE" ]; then
-        if [ $(ps -o pid --no-headers -p `cat $PIDFILE`) ]; then
-            start-stop-daemon -K -q -o --pidfile $PIDFILE --retry=TERM/20/KILL/5 >/dev/null
-	    if [ $? -eq 0 ]; then
-                rm -f "$PIDFILE"
-                log_end_msg 0
-            elif [ $? -eq 3 ]; then
-                PID=`cat $PIDFILE`
-                log_failure_msg "Failed to stop $DESC (pid $PID)"
-                exit 1
-	    fi
-        else
-            log_progress_msg "$DESC is not running but pid file exists, cleaning up"
-            rm -f "$PIDFILE"
-            log_end_msg 0
-        fi
-    else
-        log_success_msg "$DESC is not running."
-    fi
-    ;;
-  status)
-    if [ -f "$PIDFILE" ] && [ $(ps -o pid --no-headers -p `cat $PIDFILE`) ]; then
-        log_success_msg "$DESC is running with pid `cat $PIDFILE`"
-        exit 0
-    elif [ -f "$PIDFILE" ]; then
-        log_success_msg "$DESC is not running, but pid file exists."
-        exit 1
-    else
-        log_success_msg "$DESC is not running."
-        exit 3
-    fi
-    ;;
-  restart|force-reload)
-    if [ -f "$PIDFILE" ]; then
-        $0 stop
-        sleep 1
-    fi
-    $0 start
+  restart)
+    ${0} stop
+    sleep 1
+    ${0} start
     ;;
   *)
-    log_success_msg "Usage: $0 {start|stop|restart|force-reload|status}"
+    log_success_msg "Usage: $0 {start|stop|restart}"
     exit 1
     ;;
 esac
